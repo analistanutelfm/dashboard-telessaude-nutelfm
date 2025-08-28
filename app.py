fa3a74f25c57b97f4d3c134effb708ba96aa5f3c
Analista Nutel <analista.nutelfm@gmail.com>
2025-08-28 14:59:40 -0300

    Ajustes na data

--- app.py
+++ app.py
@@ -8,12 +8,6 @@ from datetime import datetime
 import os
 import locale
 
-# Definir locale para formatação de números em português
-try:
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-except locale.Error:
-    st.warning("Locale 'pt_BR.UTF-8' não encontrado.")
-    locale.setlocale(locale.LC_ALL, '')
 
 # --- 2. CONFIGURAÇÃO DA PÁGINA ---
 st.set_page_config(layout="wide", page_title="Dashboard de Teleconsultorias")
